88ee8629fc11d2fb2829f9aadfb06fdb9c5db662
madhurya <madhuryar017@gmail.com>
2026-07-06 18:10:06 +0900

    Homework 1 - Updated strings

---
+++ homework1.py
@@ -0,0 +1,17 @@
+'''
+This is the homework for the very first lecture of
+Introduction to Speech Understanding.
+
+Your first homework is to change each of the two
+following strings, then add the changes to your repository,
+commit the changes, push the changes to your GitHub repo,
+and then tell the instructor the URL for your
+GitHub repo.
+
+string1 should be changed from "change this" to "test"
+string2 should be changed from "change this" to "hello"
+'''
+
+string1 = "test"
+
+string2 = "hello"
